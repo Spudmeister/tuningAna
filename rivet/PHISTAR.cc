d60c801bb1393b8c75902a52393daeae513d5f01
Andrew Evans <aevans@physics.umn.edu>
2016-08-08 07:20:30 -0500

    First compiling and running -- histo is all zeros

--- rivet/PHISTAR.cc
+++ rivet/PHISTAR.cc
@@ -1,78 +1,94 @@
+//====================================================================//=//
+//     THIS RIVET CODE IS DESIGNED TO FOLLOW THE GENPHISTAR.CC CODE
+//     WRITTEN BY A. GUDE IN HIS ZFINDER ANALYSISIN ORDER TO CALCULATE 
+//     THE PHISTAR VARIABLE WITH ELECTRONS FROM A Z BOSON DECAY
+//
+//     AUTHOR: ANDREW.CHISTOPHER.EVANS@CERN.CH CREATED: 8-AUG-2016
+//
+//====================================================================//=//
 // -*- C++ -*-
 #include "Rivet/Analysis.hh"
 #include "Rivet/Projections/FinalState.hh"
-/// @todo Include more projections as required, e.g. ChargedFinalState, FastJets, ZFinder...
+#include "Rivet/Projections/ChargedLeptons.hh"
+#include "Rivet/Projections/NeutralFinalState.hh"
+#include "Rivet/Projections/IdentifiedFinalState.hh"
+
 
 namespace Rivet {
 
-
   class PHISTAR : public Analysis {
   public:
+    // Constructor
+    PHISTAR() : Analysis("PHISTAR") {
+    }
 
-    /// Constructor
-    PHISTAR()
-      : Analysis("PHISTAR")
-    {    }
-
-
-    /// @name Analysis methods
-    //@{
-
-    /// Book histograms and initialise projections before the run
+  public:
+    // Book histograms and initialise projections before the run
     void init() {
+      Cut c_electrons   = (Cuts::pT>9*GeV) & (Cuts::abseta<3.0); 
+      IdentifiedFinalState electrons(c_electrons);
+      electrons.acceptId(PID::ELECTRON);
+      addProjection(electrons, "ELECFS");
+      
 
-      /// @todo Initialise and register projections here
-      const ChargedFinalState cfs(-2.4, 2.4);
 
-      addProjection(cfs, "CFS");
-
-      /// @todo Book histograms here, e.g.:
-      // _h_XXXX = bookProfile1D(1, 1, 1);
-      _h_phistar = bookHisto1D("Phistar", 100, 0.0, 4.0);
-
+      _hist_phistar           = bookHisto1D(1, 1, 1);  // photon transverse energy
     }
 
+    //Phistar calculator
+    double ReturnPhistar(const double& eta0, const double& phi0, const double& eta1, const double& phi1) {
+      /* Calculate phi star */
+      static const double PI = 3.14159265358979323846;
+      double dphi = phi0 - phi1;
+    
+      // Properly account for the fact that 2pi == 0.
+      if (dphi < 0){
+          if (dphi > -PI){
+              dphi = fabs(dphi);
+          }
+          if (dphi < -PI) {
+              dphi += 2*PI;
+          }
+      }
+      if (dphi > PI){
+          dphi = 2*PI - dphi;
+      }
+    
+      const double DETA = fabs(eta0 - eta1);
+    
+      /* PhiStar */
+      return ( 1 / cosh( DETA / 2 ) ) * (1 / tan( dphi / 2 ) );
+    }
 
-    /// Perform the per-event analysis
+    // Perform the per-event analysis
     void analyze(const Event& event) {
-      const double weight = event.weight();
-      const ChargedFinalState& charged = applyProjection <ChargedFinalState>(event ,"CFS");
-      /// @todo Do the event by event analysis here
 
-    }
+      const double weight = event.weight();
+
+
+      Particles electrons = applyProjection<IdentifiedFinalState>(event, "ELECFS").particlesByPt();
+      //Now we make event cuts
+      if (electrons.size()<2) vetoEvent;
+      if (electrons[0].pT()/GeV < 30) vetoEvent;
+      if (electrons[1].pT()/GeV < 20) vetoEvent;
+      if (electrons[0].charge()*electrons[1].charge()>0) vetoEvent;
+      double M = (electrons[0].momentum() + electrons[1].momentum()).mass()/GeV;
+      if (M<60 || M >120) vetoEvent;
+      if (electrons[0].momentum().abseta() > 2.1) vetoEvent;
+      if (electrons[1].momentum().eta() > 2.4) vetoEvent;
+
+      _hist_phistar->fill(ReturnPhistar(electrons[0].momentum().eta(), electrons[0].momentum().phi(), electrons[1].momentum().eta(), electrons[1].momentum().phi()));
+    } 
 
 
     /// Normalise histograms etc., after the run
     void finalize() {
-
-      /// @todo Normalise, scale and otherwise manipulate histograms here
-
-      // scale(_h_YYYY, crossSection()/sumOfWeights()); // norm to cross section
-      // normalize(_h_YYYY); // normalize to unity
-
+      scale(_hist_phistar,       crossSection() / sumOfWeights());
     }
 
-    //@}
-
-
   private:
-
-    // Data members like post-cuts event weight counters go here
-
-
-    /// @name Histograms
-    //@{
-    Profile1DPtr _h_XXXX;
-    Histo1DPtr _h_YYYY;
-    //@}
-
-
+    Histo1DPtr  _hist_phistar;
   };
 
-
-
-  // The hook for the plugin system
   DECLARE_RIVET_PLUGIN(PHISTAR);
-
-
 }
