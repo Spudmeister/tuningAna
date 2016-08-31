c4d3c47638a7bdd8b6309f885764c044af7d4f19
Andrew Evans <aevans@physics.umn.edu>
2016-08-31 10:28:10 -0500

    Trying to figure out how to get log scale x axis

--- rivet/PHISTAR.cc
+++ rivet/PHISTAR.cc
@@ -29,10 +29,15 @@ namespace Rivet {
       IdentifiedFinalState electrons;
       electrons.acceptIdPair(PID::ELECTRON);
       addProjection(electrons, "Electrons");
+      static const double bins[] = {0.000, 0.004, 0.008, 0.012, 0.016, 0.020, 0.024, 0.029, 0.034, 0.039,
+                                     0.045, 0.052, 0.057, 0.064, 0.072, 0.081, 0.091, 0.102, 0.114, 0.128,
+                                     0.145, 0.165, 0.189, 0.219, 0.258, 0.312, 0.391, 0.524, 0.695, 0.918,
+                                     1.153, 1.496, 1.947, 2.522, 3.277,5.0,10.0};
+      std::vector<double> bins_ (bins, bins + sizeof(bins) / sizeof(bins[0]) );
 
-
-      _hist_phistar           = bookHisto1D("Phistar",30, 0.0, 3.0);  // phistar
+      _hist_phistar           = bookHisto1D("Phistar", bins_);  // phistar
     }
+
 
     //Phistar calculator
     double ReturnPhistar(const double& eta0, const double& phi0, const double& eta1, const double& phi1) {
